6566f811a90a37aea0f4333321c69236b4095d99
Abe <934982126@qq.com>
2021-08-24 18:51:34 +0800

    Add routers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FundComponent } from './fund/fund.component';
+import { HomeComponent } from './home/home.component';
+import { ManagerComponent } from './manager/manager.component';
+import { PositionComponent } from './position/position.component';
+import { SecurityComponent } from './security/security.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'fund', component: FundComponent },
+  { path: 'manager', component: ManagerComponent },
+  { path: 'security', component: SecurityComponent },
+  { path: 'position', component: PositionComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FundComponent } from './fund/fund.component';
+import { ManagerComponent } from './manager/manager.component';
+import { PositionComponent } from './position/position.component';
+import { SecurityComponent } from './security/security.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [					
+    AppComponent,
+      FundComponent,
+      ManagerComponent,
+      PositionComponent,
+      SecurityComponent,
+      HomeComponent
+   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule, ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
