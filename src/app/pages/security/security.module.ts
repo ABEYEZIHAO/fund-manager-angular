bf1cd4c886f20dae864571109bca514af93da44f
Abe <934982126@qq.com>
2021-08-25 10:35:07 +0800

    Update security.module.ts

--- src/app/pages/security/security.module.ts
+++ src/app/pages/security/security.module.ts
@@ -5,10 +5,11 @@ import { SecurityRoutingModule } from './security-routing.module';
 import { SecurityComponent } from './security.component';
 import { NzTableModule } from 'ng-zorro-antd/table';
 import { NzDividerModule } from 'ng-zorro-antd/divider';
+import { CommonModule } from '@angular/common';
 
 
 @NgModule({
-  imports: [SecurityRoutingModule, NzTableModule, NzDividerModule],
+  imports: [SecurityRoutingModule, NzTableModule, NzDividerModule, CommonModule],
   declarations: [SecurityComponent],
   exports: [SecurityComponent]
 })
